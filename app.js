29064f8a06c443f9c9692e39f2375cc4504bbe29
Adams kenny <kehindeadams72@gmail.com>
2024-02-25 04:45:08 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//Calculator program
+const display = document.getElementById('display')
+
+//append to display
+function appendToDisplay(input) {
+    display.value += input
+}
+
+//clearDisplay
+function clearDisplay() {
+  display.value =''
+}
+
+//Calculate
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Error'
+    }
+}
